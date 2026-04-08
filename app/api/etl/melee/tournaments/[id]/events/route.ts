d8d1a246e93f93138b18d5833ad90399ea3b7f47
Brian Leonard <btskynrd@gmail.com>
2026-04-07 23:24:29 -0500

    update

--- app/api/etl/melee/tournaments/[id]/events/route.ts
+++ app/api/etl/melee/tournaments/[id]/events/route.ts
@@ -1,6 +1,11 @@
-export async function GET(_, { params }) {
-  const { id } = params;
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
   const res = await fetch(`https://api.melee.gg/v1/tournaments/${id}/events`);
   const data = await res.json();
+
   return Response.json(data);
 }
