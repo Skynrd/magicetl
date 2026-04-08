930111282cc07a95873b4ab021e3403c09fb5dfe
Brian Leonard <btskynrd@gmail.com>
2026-04-07 23:26:12 -0500

    update

--- app/api/etl/melee/tournaments/[id]/events/route.ts
+++ app/api/etl/melee/tournaments/[id]/events/route.ts
@@ -1,8 +1,10 @@
+import { NextRequest } from "next/server";
+
 export async function GET(
-  request: Request,
+  _req: NextRequest,
   context: { params: { id: string } }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params; // works whether it's a Promise or not
 
   const res = await fetch(`https://api.melee.gg/v1/tournaments/${id}/events`);
   const data = await res.json();
